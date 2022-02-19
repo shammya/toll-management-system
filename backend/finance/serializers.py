97a7b4b0882fb871b8034b8fbb006b60b8b4247e
sourov <1705072@ugrad.cse.buet.ac.bd>
2022-02-19 19:43:42 +0600

    "new branch jajo created"

--- backend/finance/serializers.py
+++ backend/finance/serializers.py
@@ -1,3 +1,5 @@
+from email.policy import default
+from msilib.schema import ServiceInstall
 from rest_framework import serializers
 
 
@@ -10,4 +12,52 @@ class SnippetSerializer(serializers.Serializer):
         title = serializers.CharField(max_length=100)
         description = serializers.CharField()
         completed = serializers.BooleanField(default=False)
+        
+        
+
+
+class TollAmount(serializers.Serializer):
+        vehicleType = serializers.CharField()
+        boothId = serializers.IntegerField()
+        amount = serializers.FloatField()
+        
+
+
+
+class Route(serializers.Serializer):
+        routeId = serializers.IntegerField()
+        vehicleRegno = serializers.CharField()
+        source = serializers.CharField()
+        destination = serializers.CharField()
+        date = serializers.DateField()
+        
+
+
+class Payment(serializers.Serializer):
+        paymentId = serializers.IntegerField()
+        vehicleRegno = serializers.CharField()
+        routeId = serializers.IntegerField()
+        amount = serializers.FloatField()
+        date = serializers.DateField()
+              
+        
+class Due(serializers.Serializer):
+        reminderId = serializers.IntegerField()
+        vehicleRegNo = serializers.CharField()
+        boothId = serializers.IntegerField()
+        dueAmount = serializers.FloatField()
+        paymentId = serializers.IntegerField()
+        date = serializers.DateField()
+        
+        
+class Admin(serializers.Serializer):
+        adminId = serializers.IntegerField()
+        adminPass = serializers.CharField()
+        adminEmail = serializers.EmailField()
+        adminName = serializers.CharField()
+        
+class PaidTollList(serializers.Serializer):
+        routeId = serializers.IntegerField()
+        tollBoothId = serializers.IntegerField()
+        isPassed = serializers.BooleanField(default=False)
         
