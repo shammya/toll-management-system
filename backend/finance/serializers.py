e9e009f1742ffc58f8bf8853f550740324d5ba34
mrmajumder <monsieurmajumder@gmail.com>
2022-02-19 19:35:59 +0600

    backend: 1st 7 serializers added

--- backend/finance/serializers.py
+++ backend/finance/serializers.py
@@ -1,13 +1,50 @@
 from rest_framework import serializers
 
+# for user table
+class UserSerializer(serializers.Serializer):
+        nid = serializers.CharField(max_length=50)
+        password = serializers.CharField(max_length=50)
+        name = serializers.CharField(max_length=50)
+        email = serializers.CharField(max_length=100)
+        phoneNo = serializers.CharField(max_length=20)
+        address = serializers.CharField(max_length=100)
 
-    
-class SnippetSerializer(serializers.Serializer):
-    
-        id = serializers.IntegerField()
-        # value = serializers.IntegerField()
-        
-        title = serializers.CharField(max_length=100)
-        description = serializers.CharField()
-        completed = serializers.BooleanField(default=False)
-        
+# for VehicleType table
+class VehicleTypeSerializer(serializers.Serializer):
+        vtype = serializers.CharField(max_length=50)
+
+# for Vehicle table
+class VehicleSerializer(serializers.Serializer):
+        vehicleRegNo = serializers.CharField(max_length=50)
+        ownerNID = serializers.CharField(max_length=50)
+        modelNo = serializers.CharField(max_length=50)
+        vehicleType = serializers.CharField(max_length=50)
+        balance = serializers.FloatField(default=0)
+
+# for Offer table
+class OfferSerializer(serializers.Serializer):
+        offerID = serializers.IntegerField()
+        offerType = serializers.CharField(max_length=50)
+        offerAmount = serializers.FloatField(default=0)
+        offerTime = serializers.IntegerField()
+
+# for PaymentGateway table
+class PaymentGatewaySerializer(serializers.Serializer):
+        gatewayName = serializers.CharField(max_length=50)
+        apiSource = serializers.CharField(max_length=200)
+
+# for Recharge table
+class RechargeSerializer(serializers.Serializer):
+        rechargeID = serializers.IntegerField()
+        vehicleRegNo = serializers.CharField(max_length=50)
+        gatewayName = serializers.CharField(max_length=50)
+        offerID = serializers.IntegerField()
+        amount = serializers.FloatField()
+        date = serializers.DateTimeField()
+
+# for TollBooth table
+class TollBoothSerializer(serializers.Serializer):
+        boothID = serializers.IntegerField()
+        location = serializers.CharField(max_length=50)
+        status = serializers.CharField(max_length=50)
+
