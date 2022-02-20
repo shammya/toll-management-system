df4ccc244f0dfb6066da84a80f87c9e1a8b3e0ae
Md-Mehedi <1705082@ugrad.cse.buet.ac.bd>
2022-02-20 23:15:26 +0600

    Dues page added

--- backend/finance/urls.py
+++ backend/finance/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
+
 from finance import views
 
 urlpatterns = [
-    path('', views.homepageView.as_view()),
+    path('', views.Recharge.as_view()),
     #path('<int:pk>/', views.snippet_detail),
 ]
 
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,13 +1,11 @@
 from backend.serializers import *
-
-from django.shortcuts import render, redirect
-from django.http import Http404, HttpResponse, JsonResponse
 from django.db import connection
-
-
-from rest_framework.views import APIView
+from django.http import Http404, HttpResponse, JsonResponse
+from django.shortcuts import redirect, render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 # class for the home page backend
 class Home(APIView):
@@ -17,19 +15,19 @@ class Home(APIView):
             # create a new session
             current_user = request.session['vehicle']
             # get data from db cursor
-            cursor = connection.cursor()
-            cursor.execute("SELECT * FROM Offer")
-            offers=cursor.fetchall()
+            # cursor = connection.cursor()
+            # cursor.execute("SELECT * FROM Offer")
+            # offers=cursor.fetchall()
             # convert the data from db to a py dictionary
             offersdata = []
-            for off in offers:
-                type = off[1]
-                amount = off[2]
-                time = off[3]
+            # for off in offers:
+            #     type = off[1]
+            #     amount = off[2]
+            #     time = off[3]
 
-                row = {'offerType' : type, 'offerAmount' : amount, 'offerTime' : time}
+            #     row = {'offerType' : type, 'offerAmount' : amount, 'offerTime' : time}
 
-                offersdata.append(row)
+            #     offersdata.append(row)
             # serialize the dictionary
             serializedOffers =  OfferSerializer(offersdata, many=True).data
             print(serializedOffers)
@@ -47,9 +45,11 @@ class Home(APIView):
 # class for login backend
 class Login(APIView):
     def post(self, request, format=None):
+        print("\n\n\n\n\n\n\nhijibiji")
         # get the login vehicle no and password
         vehicle = request.POST.get('vehicle')
         password = request.POST.get('password')
+        print(vehicle, password)
 
         ###########################################
         # create a dynamic query here to check if the 
@@ -60,7 +60,7 @@ class Login(APIView):
         check_user = True
         if check_user:
             # if user is logged in
-            request.session['vehicle'] = vehicle
+            # request.session['vehicle'] = vehicle
             return JsonResponse({
                 "loginSuccess": True,
                 }, safe=False)
